202a9caff4140c2f1dea265b38d95663e5487334
Worysz <woryszlukasz@gmail.com>
2023-09-11 22:13:23 +0200

    Add project files.

---
+++ EzawodyWorsonEnterpraicment/Model/PageModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EzawodyWorsonEnterpraicment.Model
+{
+    public class PageModel
+    {   
+        public int CustomerCount { get; set; }
+        public string ProductStatus { get; set; }
+        public DateOnly OrderDate { get; set; }
+        public bool LocationStatus { get; set; }
+    }
+}
