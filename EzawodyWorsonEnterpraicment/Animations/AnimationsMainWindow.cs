8228432e75308baa72332165ea2aa69e6089611a
Worysz <woryszlukasz@gmail.com>
2023-09-16 21:57:13 +0200

    Adding new columns

---
+++ EzawodyWorsonEnterpraicment/Animations/AnimationsMainWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+using System.Windows;
+
+namespace EzawodyWorsonEnterpraicment.Animations
+{
+    public class GridLengthAnimation : AnimationTimeline
+    {
+        public override Type TargetPropertyType => typeof(GridLength);
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new GridLengthAnimation();
+        }
+
+        public override object GetCurrentValue(
+            object defaultOriginValue,
+            object defaultDestinationValue,
+            AnimationClock animationClock)
+        {
+            double fromVal = ((GridLength)defaultOriginValue).Value;
+            double toVal = ((GridLength)defaultDestinationValue).Value;
+            double animatedValue = ((toVal - fromVal) * animationClock.CurrentProgress.Value) + fromVal;
+            return new GridLength(animatedValue);
+        }
+    }
+}
--- EzawodyWorsonEnterpraicment/MainWindow.xaml.cs
+++ EzawodyWorsonEnterpraicment/MainWindow.xaml.cs
@@ -12,17 +12,90 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using EzawodyWorsonEnterpraicment.Animations;
 
 namespace EzawodyWorsonEnterpraicment
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+{ 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private bool VisibilityOfColumn2 = false;
+        private bool VisibilityOfColumn3 = false;
+        private bool VisibilityOfColumn4 = false;
+
+        private void ShowColumnButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!VisibilityOfColumn2 || !VisibilityOfColumn4)
+            {
+                // Showing 3rd column and hiding 2nd and 4th
+                GridColumn2.Visibility = Visibility.Visible;
+                GridColumn3.Visibility = Visibility.Collapsed;
+                GridColumn4.Visibility = Visibility.Collapsed;
+                VisibilityOfColumn2 = true;
+                VisibilityOfColumn3 = false;
+                VisibilityOfColumn4 = false;
+            }
+            else
+            {
+                // Hiding the 2nd column
+                GridColumn2.Visibility = Visibility.Collapsed;
+                VisibilityOfColumn2 = false;
+            }
+        }
+
+        private void ShowColumnButton_Click2(object sender, RoutedEventArgs e)
+        {
+            if (!VisibilityOfColumn3 || !VisibilityOfColumn4)
+            {
+                // Showing 3rd column and hiding 2nd and 4th
+                GridColumn3.Visibility = Visibility.Visible;
+                GridColumn2.Visibility = Visibility.Collapsed;
+                GridColumn4.Visibility = Visibility.Collapsed;
+                VisibilityOfColumn3 = true;
+                VisibilityOfColumn2 = false;
+                VisibilityOfColumn4 = false;
+            }
+            else
+            {
+                // Hiding the 3rd column
+                GridColumn3.Visibility = Visibility.Collapsed;
+                VisibilityOfColumn3 = false;
+            }
+        }
+
+        private void ShowColumnButton_Click3(object sender, RoutedEventArgs e)
+        {
+            if (!VisibilityOfColumn4)
+            {
+                // Showing 4th column and hiding 2nd and 3rd
+                GridColumn4.Visibility = Visibility.Visible;
+                GridColumn2.Visibility = Visibility.Collapsed;
+                GridColumn3.Visibility = Visibility.Collapsed;
+                VisibilityOfColumn2 = false;
+                VisibilityOfColumn3 = false;
+                VisibilityOfColumn4 = true;
+            }
+            else
+            {
+                // Hiding the 4th column
+                GridColumn4.Visibility = Visibility.Collapsed;
+                VisibilityOfColumn4 = false;
+            }
+        }
+
+        private void ShowColumnButton_Click4(object sender, RoutedEventArgs e)
+        {
+            GridColumn2.Visibility = Visibility.Collapsed;
+            GridColumn3.Visibility = Visibility.Collapsed;
+            GridColumn4.Visibility = Visibility.Collapsed;
+        }
+
+
     }
 }
+
